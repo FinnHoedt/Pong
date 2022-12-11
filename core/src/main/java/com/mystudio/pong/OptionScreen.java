ca7b5146bbecb02e1cba1ccdf84991483be627ff
fhoedt <finn.hoedt@stud.htwk-leipzig.de>
2022-12-11 13:02:59 +0000

    Initialise Game-Structure to master

---
+++ core/src/main/java/com/mystudio/pong/OptionScreen.java
@@ -0,0 +1,35 @@
+package com.mystudio.pong;
+
+import org.mini2Dx.core.game.GameContainer;
+import org.mini2Dx.core.graphics.Graphics;
+import org.mini2Dx.core.screen.BasicGameScreen;
+import org.mini2Dx.core.screen.GameScreen;
+import org.mini2Dx.core.screen.ScreenManager;
+
+public class OptionScreen extends BasicGameScreen {
+    public static int ID = 2;
+    @Override
+    public void initialise(GameContainer gc) {
+
+    }
+
+    @Override
+    public void update(GameContainer gc, ScreenManager<? extends GameScreen> screenManager, float delta) {
+
+    }
+
+    @Override
+    public void interpolate(GameContainer gc, float alpha) {
+
+    }
+
+    @Override
+    public void render(GameContainer gc, Graphics g) {
+
+    }
+
+    @Override
+    public int getId() {
+        return ID;
+    }
+}
