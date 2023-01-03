8ec4d57369e8af818a1678f2a0e4c9f9bd45c06e
dhaeufel <david_zacharie.haeufel@stud.htwk-leipzig.de>
2023-01-03 23:31:06 +0100

    ball physics

--- core/src/main/java/com/mystudio/pong/Ball.java
+++ core/src/main/java/com/mystudio/pong/Ball.java
@@ -1,8 +1,37 @@
 package com.mystudio.pong;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.Texture;
+import org.mini2Dx.core.collisions.PointQuadTree;
+import org.mini2Dx.core.collisions.QuadTree;
+import org.mini2Dx.core.engine.geom.CollisionBox;
+import org.mini2Dx.core.engine.geom.CollisionPoint;
+import org.mini2Dx.core.geom.Circle;
 import org.mini2Dx.core.graphics.Graphics;
+import org.mini2Dx.core.graphics.Sprite;
+import org.mini2Dx.core.screen.BasicGameScreen;
 
 public class Ball implements GameObject{
+    private CollisionPoint point;
+    private CollisionBox ballCollisions, testCollisions;
+    private Sprite sprite;
+    private float ballPosX, ballPosY, ballDirection, ballPlusX, ballPlusY, ballSpeed, ballHeight, ballWidth;
+    boolean gameStart = false;
+    public Ball() {
+        ballPosX = 400;
+        ballPosY = 400;
+        ballHeight = 10;
+        ballWidth = 10;
+        ballSpeed = 4;
+        ballDirection = 2f;
+        point = new CollisionPoint();
+        point.set(ballPosX, ballPosY);
+        ballCollisions = new CollisionBox(point.getRenderX(), point.getRenderY(), ballWidth, ballHeight);
+        sprite = new Sprite(new Texture(Gdx.files.internal("pongBall.png")));
+        collisionTest();
+
+    }
     @Override
     public void initialise() {
 
@@ -10,16 +39,60 @@ public class Ball implements GameObject{
 
     @Override
     public void update() {
+        point.preUpdate();
+        ballStart();
+        ballCollisionsTest();
 
     }
 
+
     @Override
     public void interpolate(float alpha) {
-
+        point.interpolate(null, alpha);
     }
 
     @Override
     public void render(Graphics g) {
+        g.drawSprite(sprite, point.getRenderX(), point.getRenderY());
+    }
+
+    public void ballStart(){
+        if(Gdx.input.isButtonPressed(Input.Keys.SPACE)) {
+            gameStart = true;
+        }
+        if(gameStart) {
+            calcNewPos();
+        }
+    }
+
+    public void ballCollisionsTest(){
+        if(point.getY() + ballHeight/2 > 600 || point.getY() + ballHeight/2 <= 0){
+            ballDirection = -1*ballDirection;
+        }
+        if(point.getX() + ballHeight/2 > 600 || point.getX() + ballHeight/2 <= 0){
+            ballSpeed = -1 * ballSpeed;
+        }
 
     }
+
+
+    public void collisionTest() {
+        testCollisions = new CollisionBox(0, 500, 1000, 10);
+    }
+
+    public void calcNewPos(){
+        int sign;
+        if(ballDirection == 0) {
+            sign = 1;
+        }
+        else {
+            sign = (int) (ballDirection / Math.abs(ballDirection));
+        }
+        ballPlusX = (float) (1 / (Math.sqrt(Math.pow(ballDirection, 2) + 1)));
+        ballPlusY = (float) (Math.sqrt(-Math.pow(ballPlusX, 2) + 1) * sign);
+        ballPlusX = ballPlusX * ballSpeed;
+        ballPlusY = ballPlusY * Math.abs(ballSpeed);
+        point.set(point.getX() + ballPlusX, point.getY() + ballPlusY);
+    }
+
 }
--- core/src/main/java/com/mystudio/pong/GameScreen.java
+++ core/src/main/java/com/mystudio/pong/GameScreen.java
@@ -7,6 +7,7 @@ import org.mini2Dx.core.screen.ScreenManager;
 
 public class GameScreen extends BasicGameScreen {
     public static int ID = 3;
+    Ball ball = new Ball();
     @Override
     public void initialise(GameContainer gc) {
 
@@ -14,21 +15,22 @@ public class GameScreen extends BasicGameScreen {
 
     @Override
     public void update(GameContainer gc, ScreenManager<? extends org.mini2Dx.core.screen.GameScreen> screenManager, float delta) {
-
+        ball.update();
     }
 
     @Override
     public void interpolate(GameContainer gc, float alpha) {
-
+        ball.interpolate( alpha);
     }
 
     @Override
     public void render(GameContainer gc, Graphics g) {
-
+        ball.render(g);
     }
 
     @Override
     public int getId() {
         return ID;
     }
+
 }
