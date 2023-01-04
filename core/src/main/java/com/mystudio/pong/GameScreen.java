a389398104b35e258efb455063c389cb73c9524c
Bastian <brotsuppe2002@gmail.com>
2023-01-04 12:55:58 +0100

    Trennlinie im GameScreen erstellt

--- core/src/main/java/com/mystudio/pong/GameScreen.java
+++ core/src/main/java/com/mystudio/pong/GameScreen.java
@@ -1,5 +1,6 @@
 package com.mystudio.pong;
 
+import com.badlogic.gdx.graphics.Color;
 import org.mini2Dx.core.game.GameContainer;
 import org.mini2Dx.core.graphics.Graphics;
 import org.mini2Dx.core.screen.BasicGameScreen;
@@ -24,11 +25,24 @@ public class GameScreen extends BasicGameScreen {
 
     @Override
     public void render(GameContainer gc, Graphics g) {
-
+        generateHyphen(g);
     }
 
     @Override
     public int getId() {
         return ID;
+    }
+
+    private void generateHyphen(Graphics g) {
+        g.setColor(Color.DARK_GRAY);
+        int width = 8;
+        int count = 12;
+        int gap = 10;
+        int height = (580/count - gap);
+        int y;
+        for (int i = 1; i <= count; i++) {
+            y = (height+gap)*i - (height+gap/2);
+            g.fillRect(600 - width/2,y,width,height);
+        }
     }
 }
