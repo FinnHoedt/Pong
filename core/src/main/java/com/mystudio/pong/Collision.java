ff3c80d9264dbe88a98928bfbdbdb9126cf030c9
maurice <maurice_raymond.putzger@stud.htwk-leipzig.de>
2023-01-10 21:31:15 +0100

    collision fix

--- core/src/main/java/com/mystudio/pong/Collision.java
+++ core/src/main/java/com/mystudio/pong/Collision.java
@@ -4,7 +4,7 @@ import com.badlogic.gdx.Gdx;
 import org.mini2Dx.core.engine.geom.CollisionBox;
 
 /**
- * collision physiks for ball
+ * collision physics for ball
  */
 public class Collision {
 
@@ -13,14 +13,15 @@ public class Collision {
     private Score score;
     private CollisionBox borderTop;
     private CollisionBox borderBottom;
+    private Boolean platformCollision = true;
 
     public Collision(Platform platformA, Platform platformB, Ball ball, Score score) {
         this.platformA = platformA;
         this.platformB = platformB;
         this.ball = ball;
         this.score = score;
-        borderTop = new CollisionBox(0, 0, Gdx.graphics.getWidth(),0);
-        borderBottom = new CollisionBox(0, Gdx.graphics.getHeight(), Gdx.graphics.getWidth(),0);
+        borderTop = new CollisionBox(0, -50, Gdx.graphics.getWidth(),50);
+        borderBottom = new CollisionBox(0, Gdx.graphics.getHeight(), Gdx.graphics.getWidth(),50);
     }
 
     /**
@@ -30,6 +31,7 @@ public class Collision {
         checkScoreCollision();
         checkPlatformCollision();
         checkBorderCollision();
+        checkBallX();
     }
 
     /**
@@ -49,13 +51,52 @@ public class Collision {
     }
 
     /**
-     * check collision with platform
+     * sets Boolean platformCollision true when ball is in center
      */
+    private void checkBallX() {
+        float ballX = ball.getBallCollision().getX();
+        float width = Gdx.graphics.getWidth()/2;
+        if(ballX >= width -50 && ballX <= width + 50 && !platformCollision) {
+            platformCollision = true;
+        }
+    }
+
+
+    /**
+     * checks collision with platforms if Boolean platformCollision is true
+     */
+    private void checkPlatformCollision() {
+        if(platformCollision) {
+            checkPlatformA();
+            checkPlatformB();
+        }
+    }
+    /*
     private void checkPlatformCollision() {
         if (platformA.getCollisionBox().intersects(ball.getBallCollision())){
             ball.changeHorizontalDirection();
         } else if(platformB.getCollisionBox().intersects(ball.getBallCollision())) {
             ball.changeHorizontalDirection();
+        }
+    }*/
+
+    /**
+     * check collision with platform A
+     */
+    private void checkPlatformA() {
+        if (platformA.getCollisionBox().intersects(ball.getBallCollision())) {
+            ball.changeHorizontalDirection();
+            platformCollision = false;
+        }
+    }
+
+    /**
+     * check collision with platform B
+     */
+    private void checkPlatformB() {
+        if(platformB.getCollisionBox().intersects(ball.getBallCollision())) {
+            ball.changeHorizontalDirection();
+            platformCollision = false;
         }
     }
 
